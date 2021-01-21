78906d04493dc152e65767cd58799663c9dd1bb5
Frank Denis <github@pureftpd.org>
2021-01-21 22:38:51 +0100

    Remove wiggle support for the hostcalls implementation

--- implementations/hostcalls/rust/src/lib.rs
+++ implementations/hostcalls/rust/src/lib.rs
@@ -22,9 +22,6 @@ mod signatures;
 mod symmetric;
 mod version;
 
-#[cfg(feature = "wiggle")]
-mod wiggle_interfaces;
-
 use std::collections::HashMap;
 use std::rc::Rc;
 
@@ -37,7 +34,7 @@ use secrets_manager::*;
 use signatures::*;
 use symmetric::*;
 
-pub use asymmetric_common::{KeyPairEncoding, PublicKeyEncoding};
+pub use asymmetric_common::{KeyPairEncoding, PublicKeyEncoding, SecretKeyEncoding};
 pub use error::CryptoError;
 pub use handles::Handle;
 pub use signatures::SignatureEncoding;
